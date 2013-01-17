853d72b0368b916f1c966c2197a68a23cb4e60e1
Carsten Gehling <carsten@sarum.dk>
2013-01-17 12:59:32 +0100

    Removing not-needed dependencies

--- route_downcaser.gemspec
+++ route_downcaser.gemspec
@@ -15,8 +15,4 @@ Gem::Specification.new do |s|
 
   s.files = Dir["{lib}/**/*"] + ["MIT-LICENSE", "Rakefile", "README.rdoc"]
   s.test_files = Dir["test/**/*"]
-
-  s.add_dependency "rails", ">= 3.0.0"
-
-  s.add_development_dependency "sqlite3"
 end
--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,8 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "rails/test_unit/railtie"
+require "sprockets/railtie"
 
-Bundler.require
+# Auto-require default libraries and those for the current Rails environment. 
+Bundler.require :default, Rails.env
+
 require "route_downcaser"
 
 module Dummy
@@ -47,7 +53,7 @@ module Dummy
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    #config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
