19e8177c777eadfccb8a8f495f56a49f90eb3057
Amiel Martin <amiel.martin@gmail.com>
2013-08-27 16:55:16 -0700

    Disable automatic loading of middleware; it is impossible to customize that way

--- lib/route_downcaser/railtie.rb
+++ lib/route_downcaser/railtie.rb
@@ -1,8 +1,8 @@
 module RouteDowncaser
   class Railtie < Rails::Railtie
-    initializer "add_downcase_route_middleware" do |app|
-      app.config.middleware.use "RouteDowncaser::DowncaseRouteMiddleware"
-    end
+    # initializer "add_downcase_route_middleware" do |app|
+    #   app.config.middleware.use "RouteDowncaser::DowncaseRouteMiddleware"
+    # end
   end
 end
 
