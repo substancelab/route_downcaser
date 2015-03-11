fa54e5c75bcc9f6b813e312d90cf73ad2da4ec1f
Carsten Gehling <carsten@sarum.dk>
2015-03-11 22:42:08 +0100

    Re-enabled code climate

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
-#require "codeclimate-test-reporter"
-#CodeClimate::TestReporter.start
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
