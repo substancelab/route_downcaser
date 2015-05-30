660d27ed8f0a2cd32aa9207c61da33623b931480
Carsten Gehling <carsten@sarum.dk>
2015-05-30 16:08:48 +0200

    .clone only copies the instance variables and not the referenced objects, which leads to problems with devise

--- lib/route_downcaser/railtie.rb
+++ lib/route_downcaser/railtie.rb
@@ -1,11 +1,7 @@
 module RouteDowncaser
   class Railtie < Rails::Railtie
     initializer "add_downcase_route_middleware" do |app|
-      # For some reason, RouteDowncaser needs to be inserted before Devise/Warden, if Devise is used
-      # But since it is not possible to test for the presence of a specific middleware module,
-      # we instead insert RouteDowncaser before a middleware module, that a) is always present
-      # and b) is inserted before Devise/Warden is
-      app.config.middleware.insert_before 'Rack::Head', 'RouteDowncaser::DowncaseRouteMiddleware'
+      app.config.middleware.use 'RouteDowncaser::DowncaseRouteMiddleware'
     end
   end
 end
