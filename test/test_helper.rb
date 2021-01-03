204735bdf1230ad3e0929b6d8210f82c1ab85a32
Jakob Skjerning <jakob@mentalized.net>
2021-01-03 16:43:11 +0100

    Prefer __dir__ over __FILE__

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,7 @@ SimpleCov.start
 ENV['RAILS_ENV'] = 'test'
 
 # require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-require File.expand_path('../rails610/config/environment.rb', __FILE__)
+require File.expand_path('rails610/config/environment.rb', __dir__)
 require 'rails/test_help'
 
 Rails.backtrace_cleaner.remove_silencers!
@@ -17,5 +17,5 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path('../fixtures', __FILE__)
+  ActiveSupport::TestCase.fixture_path = File.expand_path('fixtures', __dir__)
 end
