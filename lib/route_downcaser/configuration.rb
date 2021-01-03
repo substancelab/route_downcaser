a64d702855b36b62bc8439c7e62ed1eefa65a264
Jakob Skjerning <jakob@mentalized.net>
2021-01-03 16:57:29 +0100

    Prefer nested module/classes

--- lib/route_downcaser/configuration.rb
+++ lib/route_downcaser/configuration.rb
@@ -1,27 +1,29 @@
 # frozen_string_literal: true
 
-module RouteDowncaser::Configuration
-  def configuration
-    yield self
-  end
-
-  def define_setting(name, default = nil)
-    class_variable_set("@@#{name}", default)
-
-    define_class_method "#{name}=" do |value|
-      class_variable_set("@@#{name}", value)
+module RouteDowncaser
+  module Configuration
+    def configuration
+      yield self
     end
 
-    define_class_method name do
-      class_variable_get("@@#{name}")
+    def define_setting(name, default = nil)
+      class_variable_set("@@#{name}", default)
+
+      define_class_method "#{name}=" do |value|
+        class_variable_set("@@#{name}", value)
+      end
+
+      define_class_method name do
+        class_variable_get("@@#{name}")
+      end
     end
-  end
 
-  private
+    private
 
-  def define_class_method(name, &block)
-    (class << self; self; end).instance_eval do
-      define_method name, &block
+    def define_class_method(name, &block)
+      (class << self; self; end).instance_eval do
+        define_method name, &block
+      end
     end
   end
 end
